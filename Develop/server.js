616830259ca1623127bbabbecddc5f8fa96779d1
Erik Edin <ekedin@gmail.com>
2024-01-03 19:12:49 -0700

    added middleware functions

--- Develop/server.js
+++ Develop/server.js
@@ -9,3 +9,18 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//returns notes.html
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+//get notes
+app.get("/api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "db/db.json"));
+});
+
+//returns index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
